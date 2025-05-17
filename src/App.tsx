3162afa6467444e85ef033f4d6c0c663df106e58
mirenn <ngihyt910@outlook.jp>
2025-05-17 18:59:06 +0900

    feat: プレイヤー名をlocalStorageから読み込み、変更時に保存する機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -337,6 +337,21 @@ function App() {
   const [playerName, setPlayerName] = useState('')
   const [nameError, setNameError] = useState('')
 
+  // Load player name from localStorage on initial render
+  useEffect(() => {
+    const storedPlayerName = localStorage.getItem('playerName');
+    if (storedPlayerName) {
+      setPlayerName(storedPlayerName);
+    }
+  }, []);
+
+  // Save player name to localStorage whenever it changes
+  useEffect(() => {
+    if (playerName) {
+      localStorage.setItem('playerName', playerName);
+    }
+  }, [playerName]);
+
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/[^0-9]/g, '').slice(0, 4)
     setRoomCode(value)
@@ -386,7 +401,7 @@ function App() {
                   className="text-sm text-indigo-600 hover:text-indigo-800 flex items-left"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                    <path fillRule="evenodd" d="M18 10a8 8 0 1 1 0-16 8 8 0 0 1 0 16zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                   </svg>
                 </button>
                 <div 
